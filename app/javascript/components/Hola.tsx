55ce90230662ef953d4157aa21b859036335e655
lef237 <lef237k5@gmail.com>
2023-03-06 11:04:22 +0900

    ReactのHola.tsxのファイルに型を導入した

--- app/javascript/components/Hola.tsx
+++ app/javascript/components/Hola.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 
-const Hola = ({ name = "名無し", age = 99 }) => (
+type Props = {
+  name: string;
+  age: number;
+};
+
+const Hola = ({ name = "名無し", age = 99 }: Props) => (
   <>
     <div>こんにちは {name} さん！</div>
     <div>あなたの年齢は {age} 歳です。</div>
