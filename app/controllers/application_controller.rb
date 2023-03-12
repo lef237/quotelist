d401b48f087f5180fd0722ad3dba4babebe5c71f
lef237 <lef237k5@gmail.com>
2023-03-12 18:48:11 +0900

    Lintで指摘された箇所を修正した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile_url])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name profile_url])
   end
 end
