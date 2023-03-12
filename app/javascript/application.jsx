912aefd6e440f66edfd875e38b831fe6f58cc143
lef237 <lef237k5@gmail.com>
2023-03-12 21:05:11 +0900

    mountが複数あった場合の処理をDRY化した

--- app/javascript/application.jsx
+++ app/javascript/application.jsx
@@ -8,17 +8,30 @@ Mount(Hello, "hello");
 Mount(Hey, "hey");
 
 // const holaNames = ["hola1", "hola2", "hola3", "hola4"];
-const number = 7; //本来は4だが、数字を多めにしても問題なく表示される
-const holaNames = Array.from(
-  { length: number },
-  (_, index) => `hola${index + 1}`
-);
+// const number = 7; //本来は4だが、数字を多めにしても問題なく表示される
+// const holaNames = Array.from(
+//   { length: number },
+//   (_, index) => `hola${index + 1}`
+// );
 
-holaNames.forEach((name) => {
-  Mount(Hola, name);
-});
+// holaNames.forEach((name) => {
+//   Mount(Hola, name);
+// });
 
 // Mount(Hola, "hola1");
 // Mount(Hola, "hola2");
 // Mount(Hola, "hola3");
 // Mount(Hola, "hola4");
+
+function mountComponents(component, divName, numberOfComponents = 30) {
+  const names = Array.from(
+    { length: numberOfComponents },
+    (_, index) => `${divName}${index + 1}`
+  );
+
+  names.forEach((name) => {
+    Mount(component, name);
+  });
+}
+
+mountComponents(Hola, "hola");
