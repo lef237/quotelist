d74cc90f38e049c5770ce482206dc2ef75f8ea3a
lef237 <lef237k5@gmail.com>
2023-03-14 18:22:02 +0900

    slim-lintのCIをチェックする

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 
 class TopController < ApplicationController
   def terms_of_service; end
--- spec/requests/top_spec.rb
+++ spec/requests/top_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Top', type: :request do
   describe 'GET /terms_of_service' do
     it 'HTTP ステータス 200 を返す' do
       get '/terms_of_service'
-      expect(response).to have_http_status(404)
+      expect(response).to have_http_status(:ok)
     end
   end
 
