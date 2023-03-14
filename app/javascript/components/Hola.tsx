c89567855eb387728a7663a21b13f7bf7acceeb1
lef237 <lef237k5@gmail.com>
2023-03-14 18:30:43 +0900

    TypeScriptの型チェックのCIを確認する

--- app/javascript/components/Hola.tsx
+++ app/javascript/components/Hola.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-type Props = {
-  name: string;
-  age: number;
-};
+// type Props = {
+//   name: string;
+//   age: number;
+// };
 
 const Hola = ({ name = "名無し", age = 99 }: Props) => (
   <>
