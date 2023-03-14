8bd4882825e8534a515ace0cdbbbdb5a0f80da8c
lef237 <lef237k5@gmail.com>
2023-03-14 13:32:55 +0900

    ポリシー関係のルーティングを変更した

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'top/terms_of_service'
-  get 'top/privacy_policy'
+  get 'terms_of_service', to: 'top#terms_of_service'
+  get 'privacy_policy', to: 'top#privacy_policy'
   devise_for :users
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
