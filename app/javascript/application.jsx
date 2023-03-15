6111db92789ea42a14375f2231745df1063d35c1
lef237 <lef237k5@gmail.com>
2023-03-15 19:04:34 +0900

    関数の順序をわかりやすくした

--- app/javascript/application.jsx
+++ app/javascript/application.jsx
@@ -11,16 +11,6 @@ Mount(Hey, "hey");
 // div要素のNodeListを取得
 const divElements = document.querySelectorAll('div');
 
-// 特定の単語が先頭にある、divのIDの末尾の数字の中で、最大値を取得する
-const getMaxId = (prefix, ids) => {
-  const filteredIds = ids.filter(function(id) {
-    return id.startsWith(prefix);
-  }).map(function(id) {
-    return parseInt(id.replace(prefix, ''));
-  });
-  return Math.max.apply(Math, filteredIds);
-}
-
 // NodeListからdivのidsを取得している
 const getDivIds = (divElements) => {
   const divIds = [];
@@ -30,6 +20,16 @@ const getDivIds = (divElements) => {
     }
   }
   return divIds;
+}
+
+// 特定の単語が先頭にある、divのIDの末尾の数字の中で、最大値を取得する
+const getMaxId = (prefix, ids) => {
+  const filteredIds = ids.filter(function(id) {
+    return id.startsWith(prefix);
+  }).map(function(id) {
+    return parseInt(id.replace(prefix, ''));
+  });
+  return Math.max.apply(Math, filteredIds);
 }
 
 const divIds = getDivIds(divElements);
