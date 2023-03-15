7b063a03f93f36d83b6b8a7842906d6cb999f32a
lef237 <lef237k5@gmail.com>
2023-03-15 18:24:35 +0900

    id末尾の数字を読み取って最大値を取得できるようにした

--- app/javascript/Mount.jsx
+++ app/javascript/Mount.jsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 
 const Mount = (Component, mountNodeId) => {
   const mountNode = document.getElementById(mountNodeId);
+  // 受け渡す`data`がある場合とない場合があるので、ブラウザコンソール上で「Uncaught TypeError: Cannot read properties of null (reading 'getAttribute')」が表示されても大丈夫。
+  // また、MountComponents.tsxでも使われているので、ここではまだ明確な値が入っておらず、それが上記のメッセージが表示される理由になっている。
   const props = JSON.parse(mountNode.getAttribute("data"));
   createRoot(mountNode).render(<Component {...props} />);
 };
--- app/javascript/application.jsx
+++ app/javascript/application.jsx
@@ -5,6 +5,47 @@ import Hello from "./components/Hello";
 import Hey from "./components/Hey";
 import Hola from "./components/Hola";
 
+const getMaxIdStartingWithPrefix = (prefix, ids) => {
+  const filteredIds = ids.filter(function(id) {
+    return id.startsWith(prefix);
+  }).map(function(id) {
+    return parseInt(id.replace(prefix, ''));
+  });
+  return Math.max.apply(Math, filteredIds);
+}
+
+// すべてのdiv要素を取得
+const divElements = document.getElementsByTagName("div");
+
+// console.log(divElements)
+
+// すべてのdiv要素からIDを取得
+const divIds = [];
+for (let i = 0; i < divElements.length; i++) {
+  if (divElements[i].id) {
+    divIds.push(divElements[i].id);
+  }
+}
+
+// 取得したIDの配列をコンソールに出力
+console.log(divIds);
+
+const holaMaxId = getMaxIdStartingWithPrefix('hola', divIds)
+
+console.log(holaMaxId)
+
+
+// const holaIds = divIds.filter(function(id) {
+//   return id.startsWith('hola'); // 'hello'で始まるもののみをフィルター
+// }).map(function(id) {
+//   return parseInt(id.replace('hola', '')); // 'hello'を削除し、数値に変換
+// });
+
+// const maxId = Math.max.apply(Math, holaIds); // 最大値を取得
+
+// console.log(holaIds); // [1, 2, 3, 42]
+// console.log(maxId); // 42
+
 Mount(Hello, "hello");
 Mount(Hey, "hey");
 MountComponents(Hola, "hola");
