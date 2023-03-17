184bf87c05fa945a7d57467757632983506fcba0
lef237 <lef237k5@gmail.com>
2023-03-17 13:54:46 +0900

    migrateファイルにnot null属性を追加した

--- db/migrate/20230317042345_create_books.rb
+++ db/migrate/20230317042345_create_books.rb
@@ -1,9 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[7.0]
   def change
     create_table :books do |t|
-      t.string :title
-      t.string :author
-      t.string :information_url
+      t.string :title, null: false
+      t.string :author, null: false
+      t.string :information_url, default: ""
 
       t.timestamps
     end
