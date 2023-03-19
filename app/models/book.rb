b8e0001bcb67f3513ac3892d92762099d2f2a5b8
lef237 <lef237k5@gmail.com>
2023-03-19 19:16:47 +0900

    ActiveStorageのアップロードに制限を設けた（ファイルサイズとファイルの種類）

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,24 @@
 class Book < ApplicationRecord
   has_many :quotes
   has_one_attached :avatar
+  validate :validate_avatar
+
+  private
+
+  # アバター画像のバリデーション内容
+  def validate_avatar
+    return unless avatar.attached?
+    # 画像サイズの制限
+    if avatar.blob.byte_size > 1.megabytes
+      # エラーメッセージはi18nから取得
+      errors.add(:avatar, 'ファイルサイズは1MB以下にしてください')
+    elsif !image?
+      errors.add(:avatar, 'jpg, jpeg, pngのどれかの形式でアップロードしてください')
+    end
+  end
+
+  # 拡張子でファイルの種類を確認
+  def image?
+    avatar.content_type.in?(%("image/jpeg image/jpg image/png"))
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :quotes
   has_one_attached :avatar
+  validate :validate_avatar
+
+  private
+
+  def validate_avatar
+    return unless avatar.attached?
+    if avatar.blob.byte_size > 1.megabytes
+      errors.add(:avatar, 'ファイルサイズは1MB以下にしてください')
+    elsif !image?
+      errors.add(:avatar, 'jpg, jpeg, pngのどれかの形式でアップロードしてください')
+    end
+  end
+
+  def image?
+    avatar.content_type.in?(%("image/jpeg image/jpg image/png"))
+  end
 end
