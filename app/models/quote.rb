58c90a07f17e8170f7e76b147d22e2444e430fff
lef237 <lef237k5@gmail.com>
2023-03-21 09:44:57 +0900

    引用の文字数を300文字に制限した

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,4 +5,5 @@ class Quote < ApplicationRecord
   belongs_to :book
   belongs_to :source_quote, class_name: 'Quote', optional: true, inverse_of: :child_quotes
   has_many :child_quotes, class_name: 'Quote', foreign_key: 'source_quote_id', inverse_of: :source_quote
+  validates :sentence, length: { maximum: 300 }
 end
