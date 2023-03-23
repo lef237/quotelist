747aecff4a97972ec9c8b83e0deaa6f361a7f47d
lef237 <lef237k5@gmail.com>
2023-03-23 20:19:16 +0900

    書籍名及び著者名の検索機能を作った

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,15 @@ class BooksController < ApplicationController
 
   # GET /books or /books.json
   def index
-    @books = Book.with_attached_avatar
+    keyword = params[:keyword]
+    # 先に著者名を調べてからタイトルを調べる。もし著者名がある場合には、そこでreturnする？
+    if keyword
+      @books = Book.where("author LIKE ? OR title LIKE ?", "%#{params[:keyword]}%", "%#{params[:keyword]}%").order(created_at: :desc).with_attached_avatar
+      @empty_message = "検索ワード「#{params[:keyword]}」に合致する書籍や著者は登録されていません。"
+    elsif
+      @books = Book.order(created_at: :desc).with_attached_avatar
+      @empty_message = "まだ書籍は登録されていません。"
+    end
   end
 
   # GET /books/1 or /books/1.json
