f56c6f39bd9af73a0e52a64d2739ba5b3e48ed2f
lef237 <lef237k5@gmail.com>
2023-03-28 13:19:55 +0900

    Prettierでフォーマットした

--- app/javascript/components/QuoteDelete.tsx
+++ app/javascript/components/QuoteDelete.tsx
@@ -14,19 +14,21 @@ type Props = {
 
 // 親コンポーネントの`onDelete`を使いたいときはこのように書く
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const QuoteDelete = ( {id, onDelete} : Props ) => {
-  console.log(id)
+const QuoteDelete = ({ id, onDelete }: Props) => {
+  console.log(id);
 
   // DELETEを送って削除できるようにする
   // # DELETE /quotes/1 or /quotes/1.json を参考に、IDをURLの最後に付けて.jsonを付ける
 
-  const csrfToken = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content');
+  const csrfToken = document
+    .querySelector('meta[name="csrf-token"]')
+    ?.getAttribute("content");
 
   const handleDelete = () => {
     fetch(`/quotes/${id}`, {
       method: "DELETE",
       headers: {
-        "X-CSRF-Token": csrfToken || '',
+        "X-CSRF-Token": csrfToken || "",
         "Content-Type": "application/json",
         Accept: "application/json",
       },
@@ -35,7 +37,7 @@ const QuoteDelete = ( {id, onDelete} : Props ) => {
       // 画面を更新している。状態を維持しながら画面を更新するための適切な方法ではない。ReactのuseStateフックを使用して状態を更新し、コンポーネントを再描画する方法が推奨されている。
       window.location.reload(); // 画面を更新する
       // console.logを表示させたいときは画面更新をしない
-      console.log('Deleted')
+      console.log("Deleted");
     });
   };
 
@@ -46,7 +48,6 @@ const QuoteDelete = ( {id, onDelete} : Props ) => {
       <button onClick={handleDelete}>削除</button>
     </>
   );
-
 };
 
 export default QuoteDelete;
--- app/javascript/components/QuotePost.tsx
+++ app/javascript/components/QuotePost.tsx
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 type RequestData = {
-  user_id: number | '';
-  book_id: number | '';
+  user_id: number | "";
+  book_id: number | "";
   sentence: string;
-  page_number: number | '';
-  source_quote_id: number| '';
+  page_number: number | "";
+  source_quote_id: number | "";
 };
 
 const initialState: RequestData = {
-  user_id: '',
-  book_id: '',
-  sentence: '',
-  page_number: '',
-  source_quote_id: ''
+  user_id: "",
+  book_id: "",
+  sentence: "",
+  page_number: "",
+  source_quote_id: "",
 };
 
 const QuotePost: React.FC = () => {
@@ -27,31 +27,35 @@ const QuotePost: React.FC = () => {
 
     // const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
 
-    const csrfToken = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content');
+    const csrfToken = document
+      .querySelector('meta[name="csrf-token"]')
+      ?.getAttribute("content");
 
     if (!formData) {
-      throw new Error('formData is empty');
+      throw new Error("formData is empty");
     }
 
-    const response = await fetch('/quotes', {
-      method: 'POST',
+    const response = await fetch("/quotes", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
-        'X-CSRF-Token': csrfToken || '',
+        "Content-Type": "application/json",
+        "X-CSRF-Token": csrfToken || "",
       },
       body: JSON.stringify(formData),
     });
 
     if (response.ok) {
-      console.log('Contact form submitted successfully!');
+      console.log("Contact form submitted successfully!");
       // Do something after successful submission
     } else {
-      console.error('Failed to submit contact form.');
+      console.error("Failed to submit contact form.");
       // Handle submission error
     }
   };
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     const { name, value } = event.target;
     setFormData((prevFormData) => ({
       ...prevFormData,
@@ -64,23 +68,52 @@ const QuotePost: React.FC = () => {
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="user_id">User ID:</label>
-        <input type="number" id="user_id" name="user_id" value={formData.user_id} onChange={handleChange} />
+        <input
+          type="number"
+          id="user_id"
+          name="user_id"
+          value={formData.user_id}
+          onChange={handleChange}
+        />
       </div>
       <div>
         <label htmlFor="book_id">Book ID:</label>
-        <input type="number" id="book_id" name="book_id" value={formData.book_id} onChange={handleChange} />
+        <input
+          type="number"
+          id="book_id"
+          name="book_id"
+          value={formData.book_id}
+          onChange={handleChange}
+        />
       </div>
       <div>
         <label htmlFor="sentence">Sentence:</label>
-        <textarea id="sentence" name="sentence" value={formData.sentence} onChange={handleChange} />
+        <textarea
+          id="sentence"
+          name="sentence"
+          value={formData.sentence}
+          onChange={handleChange}
+        />
       </div>
       <div>
         <label htmlFor="page_number">Page Number:</label>
-        <input type="number" id="page_number" name="page_number" value={formData.page_number} onChange={handleChange} />
+        <input
+          type="number"
+          id="page_number"
+          name="page_number"
+          value={formData.page_number}
+          onChange={handleChange}
+        />
       </div>
       <div>
         <label htmlFor="source_quote_id">Source Quote ID:</label>
-        <input type="number" id="source_quote_id" name="source_quote_id" value={formData.source_quote_id} onChange={handleChange} />
+        <input
+          type="number"
+          id="source_quote_id"
+          name="source_quote_id"
+          value={formData.source_quote_id}
+          onChange={handleChange}
+        />
       </div>
       <button type="submit">Submit</button>
     </form>
