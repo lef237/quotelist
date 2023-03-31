e7c06aad13afce3ab4d976bb08147c9ee616c380
lef237 <lef237k5@gmail.com>
2023-03-31 13:38:20 +0900

    ログインしていない場合のゲストボタンを作成した

--- app/javascript/application.jsx
+++ app/javascript/application.jsx
@@ -14,6 +14,7 @@ import QuotesCount from "./components/QuotesCount";
 import QuotePost from "./components/QuotePost";
 import QuoteDelete from "./components/QuoteDelete";
 import CoquoteButton from "./components/CoquoteButton";
+import GuestCoquoteButton from "./components/GuestCoquoteButton";
 
 const divElements = document.querySelectorAll("div");
 
@@ -27,3 +28,4 @@ MountComponents(QuotesCount, "quotescount", divElements);
 MountComponents(QuotePost, "quotepost", divElements);
 MountComponents(QuoteDelete, "quotedelete", divElements);
 MountComponents(CoquoteButton, "coquotebutton", divElements);
+MountComponents(GuestCoquoteButton, "guestcoquotebutton", divElements);
---
+++ app/javascript/components/GuestCoquoteButton.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+type Props = {
+  quoteId: number;
+  numberCoquoted: number;
+}
+
+const GuestCoquoteButton = ({ quoteId, numberCoquoted }: Props) => {
+  const handleClick = () => {
+    alert('ログインしてください');
+  }
+
+  return (
+    <>
+      <button onClick={handleClick}>
+        リツイートボタン
+      </button>
+      <br />
+      <a href={`/quotes/${quoteId}`}>{numberCoquoted}人に引用されています</a>
+    </>
+  );
+}
+
+export default GuestCoquoteButton;
