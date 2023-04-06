4b7770df0575f7d03e9a52784ed1a184efbc3c1c
lef237 <lef237k5@gmail.com>
2023-04-06 23:20:49 +0900

    Rubocopを通した。文字列の途中では改行できないため、設定を見直した。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,11 +13,11 @@ module ApplicationHelper
 
   def twitter_card_tags
     {
-      "twitter:card" => "summary_large_image",
-      "twitter:site" => "@lef237",
-      "twitter:title" => content_for?(:title) ? content_for(:title) : "引用箱 QuoteList",
-      "twitter:description" => content_for?(:description) ? content_for(:description) : "未知の書籍と出会う切っ掛けとして、色んな本の引用を閲覧・紹介することができます！ぜひ、色んな引用をクリックして、お気に入りの本を見つけてみましょう",
-      "twitter:image" => content_for?(:image) ? content_for(:image) : asset_path("quotelistogp.png")
+      'twitter:card' => 'summary_large_image',
+      'twitter:site' => '@lef237',
+      'twitter:title' => content_for?(:title) ? content_for(:title) : '引用箱 QuoteList',
+      'twitter:description' => content_for?(:description) ? content_for(:description) : '未知の書籍と出会う切っ掛けとして、色んな本の引用を閲覧・紹介することができます！ぜひ、色んな引用をクリックして、お気に入りの本を見つけてみましょう',
+      'twitter:image' => content_for?(:image) ? content_for(:image) : asset_path('quotelistogp.png')
     }
   end
 end
