f9de8b90edf1b1e9f53d96203cb8fc88752c7fcc
lef237 <lef237k5@gmail.com>
2023-04-06 21:52:42 +0900

    OGPでタイトルや画像が表示されるようにした

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def ogp_tags
+    {
+      "og:title" => content_for?(:title) ? content_for(:title) : "引用箱 QuoteList",
+      "og:type" => "website",
+      "og:description" => content_for?(:description) ? content_for(:description) : "未知の書籍と出会う切っ掛けとして、色んな本の引用を閲覧・紹介することができます！ぜひ、色んな引用をクリックして、お気に入りの本を見つけてみましょう",
+      "og:url" => request.original_url,
+      "og:image" => asset_path("quotelistogp.png")
+    }
+  end
 end
