d29432461c4a190f94286b44be6990e13fc842e8
lef237 <lef237k5@gmail.com>
2023-04-06 16:42:25 +0900

    ブラウザバックだけでなく、進むボタンを押したときにもReactが更新されるようにした

--- app/javascript/controllers/reload_controller.js
+++ app/javascript/controllers/reload_controller.js
@@ -2,8 +2,18 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   connect() {
-    document.addEventListener("turbo:before-cache", () => {
-      window.location.reload();
-    });
+    this.reloadPage = this.reloadPage.bind(this);
+
+    window.addEventListener("popstate", this.reloadPage);
+    document.addEventListener("turbo:before-cache", this.reloadPage);
+  }
+
+  disconnect() {
+    window.removeEventListener("popstate", this.reloadPage);
+    document.removeEventListener("turbo:before-cache", this.reloadPage);
+  }
+
+  reloadPage() {
+    window.location.reload();
   }
 }
