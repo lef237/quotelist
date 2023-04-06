05b8510e0ca95da1e66f0e26346f4555e9c1cc2f
lef237 <lef237k5@gmail.com>
2023-04-06 23:10:23 +0900

    TwitterCardを設定した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,14 @@ module ApplicationHelper
       'og:image' => asset_path('quotelistogp.png')
     }
   end
+
+  def twitter_card_tags
+    {
+      "twitter:card" => "summary_large_image",
+      "twitter:site" => "@lef237",
+      "twitter:title" => content_for?(:title) ? content_for(:title) : "引用箱 QuoteList",
+      "twitter:description" => content_for?(:description) ? content_for(:description) : "未知の書籍と出会う切っ掛けとして、色んな本の引用を閲覧・紹介することができます！ぜひ、色んな引用をクリックして、お気に入りの本を見つけてみましょう",
+      "twitter:image" => content_for?(:image) ? content_for(:image) : asset_path("quotelistogp.png")
+    }
+  end
 end
