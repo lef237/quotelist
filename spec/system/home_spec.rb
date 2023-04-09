096b5d6f87546a8f957f075bb461da38d9f3690f
lef237 <lef237k5@gmail.com>
2023-04-09 21:00:39 +0900

    代表的なページのシステムテストを実装した

---
+++ spec/system/home_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'Home', type: :system do
+  let!(:user) { create(:user) }
+  let!(:book) { create(:book) }
+  let!(:quote) { create(:quote, user: user, book: book) }
+
+  before do
+    driven_by(:rack_test)
+    visit root_path
+  end
+
+  it 'displays the page title' do
+    expect(page).to have_content('引用箱')
+  end
+
+  it 'displays the introduction text' do
+    expect(page).to have_content('未知の書籍と出会う切っ掛けとして、色んな本の引用を閲覧・紹介することができます！')
+    expect(page).to have_content('ぜひ、色んな引用をクリックして、お気に入りの本を見つけてみましょう📚🔍')
+  end
+
+  it 'displays quotes' do
+    expect(page).to have_content(quote.sentence)
+  end
+
+  it 'displays navigation buttons' do
+    expect(page).to have_link('ランダム表示')
+    expect(page).to have_link('人気順で表示')
+  end
+
+  it 'displays footer links' do
+    expect(page).to have_link('利用規約')
+    expect(page).to have_link('プライバシーポリシー')
+  end
+end
