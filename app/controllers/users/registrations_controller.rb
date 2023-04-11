c753cfc874050e883953e76537b59e5564d7ef1f
lef237 <lef237k5@gmail.com>
2023-04-11 09:07:06 +0900

    Rubocopの指摘を受けて引数の書き方を変えた

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -43,7 +43,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   protected
 
-  def after_sign_up_path_for(resource)
+  def after_sign_up_path_for(_resource)
     new_user_registration_path
   end
 
