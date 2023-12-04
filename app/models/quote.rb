27a5c8217914a3ffa1dca7bcc2ae728b3033bd2a
lef237 <lef237k5@gmail.com>
2023-12-04 21:38:55 +0900

    Prevent unlettered submissions of quotes

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,7 +5,7 @@ class Quote < ApplicationRecord
   belongs_to :book
   belongs_to :source_quote, class_name: 'Quote', optional: true, inverse_of: :child_quotes
   has_many :child_quotes, class_name: 'Quote', foreign_key: 'source_quote_id', inverse_of: :source_quote, dependent: :restrict_with_exception
-  validates :sentence, length: { maximum: 300 }
+  validates :sentence, length: { maximum: 300, minimum: 1 }
 
   def self.popular
     joins('LEFT OUTER JOIN quotes AS child_quotes ON quotes.id = child_quotes.source_quote_id')
