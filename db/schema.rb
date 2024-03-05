445c642d199b7d4bb7eaf7e7aa840a2353a31f1f
lef237 <lef237k5@gmail.com>
2024-03-05 22:35:02 +0900

    Add admin boolean to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_19_034736) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_05_133152) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,6 +73,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_19_034736) do
     t.datetime "updated_at", null: false
     t.string "name", default: "", null: false
     t.string "profile_url"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
