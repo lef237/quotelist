8d29ba9993d4f7aa3e10a7dcf5a9acb08d43ebba
lef237 <lef237k5@gmail.com>
2025-09-17 18:52:34 +0900

    Add email authentication and updated Devise settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
   has_many :quotes, dependent: :destroy
   has_one_attached :avatar
   validate :validate_avatar
